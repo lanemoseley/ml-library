6ce2d6ae98bbb698494d48adbf46bc0eedb4f248
Lane Moseley <moseley.lane@gmail.com>
2020-11-20 16:29:58 -0700

    Adding new files.

---
+++ kNN.py
@@ -0,0 +1,8 @@
+# shouldn't be that hard at least the straigh k-NN going in from the beginning
+# given S and given point, compare to S and return majority (must set k first)
+# fancy version:  allow user to decide distance measure (euclidean, l-inf, l-1, etc)
+#                 use specific k
+# data?  binary data (iris data)
+
+
+
